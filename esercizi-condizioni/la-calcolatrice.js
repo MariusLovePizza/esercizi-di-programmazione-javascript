7a64df91474cc3d00222470a308e1f5d52247131
MariusLovesPizza <marius.coanta@gmail.com>
2020-03-02 11:43:54 +0100

    Commit-02-03

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -28,3 +28,69 @@
 
   http://www.imparareaprogrammare.it
 */
+var num1= Math.floor(Math.random()*99 +1);
+var num2= Math.floor(Math.random()*99 +1);
+var int_opp = Math.floor(Math.random()*6 +1);
+var str_opp;
+
+console.log("n1: "+ num1+", n2: "+ num2+", int_opp1: "+ int_opp);
+
+switch (int_opp) {
+  case 1:
+    str_opp = "somma";
+    break;
+
+  case 2:
+    str_opp = "sottrazione";
+    break;
+
+  case 3:
+    str_opp = "moltiplicazione";
+    break;
+
+  case 4:
+    str_opp = "divisione";
+    break;
+
+  case 5:
+    str_opp = "modulo";
+    break;
+  case 6:
+    str_opp = "potenza";
+     break;
+     case 7:
+       str_opp = "media";
+       break;
+     default:
+
+        break
+}
+
+console.log("Operazione:" + str_opp+"\n\n");
+
+switch (str_opp) {
+  case "somma":
+    var s=num1+num2;
+    console.log("Somma: " + s);
+    break;
+  case "sottrazione":
+    var s=num1-num2;
+    console.log("sottrazione: " + s);
+    break;
+  case "moltiplicazione":
+    var s=num1*num2;
+    console.log("moltiplicazione: " + s);
+    break;
+  case "divisione":
+     var s=num1/num2;
+     console.log("Somma: " + s);
+     break;
+  case "somma":
+    var s=num1+num2;
+    console.log("Somma: " + s);
+    break;
+
+  default:
+    console.log("La faticaa...")
+    break
+}
