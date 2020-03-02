3ed60b3c352eb2681f9cc81e5dfde615aaf6dbe2
MariusLovesPizza <marius.coanta@gmail.com>
2020-03-02 15:46:49 +0100

    Update rombo-che-stampa.js

--- esercizi-cicli/rombo-che-stampa.js
+++ esercizi-cicli/rombo-che-stampa.js
@@ -16,10 +16,11 @@
 
   http://www.imparareaprogrammare.it
 */
-
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm 
 var input = Math.floor(Math.random()*9+1);
 var tab;
 var string;
+var t="\t";
 
 if((input&2)==0){
   tab = input /2;
@@ -28,8 +29,9 @@ if((input&2)==0){
 else{tab = (input-1)/2;}
 console.log("Numero dispari: "+input);
 
-for(var i=1; i<=input;i++){
-  
+for(var i=1; i<=(tab+1);i++){ // nr ri righe
+  for(var j=0; j<=input;j+=2){
 
+  }
 
 }
